efbae2b92e43634250c981a73a4754f96a8630ae
dabdab59 <124675225+dabdab59@users.noreply.github.com>
2025-01-09 00:29:24 +0100

    finish

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from pere_fouras_challenge import*
-from chance_challenges import*
-from logical_challenges import*
-from final_challenge import*
-from math_challenges import*
-from utility_functions import*
+running = True
+from pere_fouras_challenge import *
+from chance_challenges import *
+from logical_challenges import *
+from final_challenge import *
+from math_challenges import *
+from game_state import *
 
 
 def main_menu():
@@ -13,36 +14,32 @@ def main_menu():
         print("2. Chance Challenges")
         print("3. Logical Challenge")
         print("4. Pere Fouras Challenge")
-        print("5. Leave Menu")
+        print("5. Final Challenge")
+        print("6. Leave Menu")
         print("==========================")
 
         choice = input("Enter Your choice : ").strip()
 
         if choice == "1":
             print("\n>>> Starting Math challenges...")
-            selected_player = choose_player(team)
-            math_challenge(selected_player)
+            math_challenge()
         elif choice == "2":
             print("\n>>> Starting Chance Challenges...")
-            selected_player = choose_player(team)
-            chance_challenge(selected_player)
+            chance_challenge()
         elif choice == "3":
             print("\n>>> Starting Logical Challenge...")
-            selected_player = choose_player(team)
-            battleship_game(selected_player)
+            battleship_game()
         elif choice == "4":
             print("\n>>> Starting Pere Fouras Challenge...")
-            selected_player = choose_player(team)
-            pere_fouras_riddles(selected_player)
+            pere_fouras_riddles()
         elif choice == "5":
+            print("\n>>> Starting Final Challenge...")
+            treasure_room()
+        elif choice == "6":
             print("\nThank you for playing! See you soon!")
             break
         else:
             print("\nImpossible choice! Chose a number between 1 and 5!")
 
-team = compose_equipe()
-for player in team:
-    print(player)
+
 main_menu()
-
-
